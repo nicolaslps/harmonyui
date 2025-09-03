359134352683b8b3a7e95bc1e9584c499d23c25d
nicolaslps <lopesn.pro@gmail.com>
2025-09-03 21:26:31 +0200

    first version of the landing

--- apps/docs/config/bundles.php
+++ apps/docs/config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     HarmonyUi\Bundle\HarmonyUiBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
 ];
