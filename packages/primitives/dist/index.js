6e3771c2ccac1b301f70ce115ea5ee590c9968f0
nicolaslps <lopesn.pro@gmail.com>
2025-09-15 23:55:34 +0200

    rework

--- packages/primitives/dist/index.js
+++ packages/primitives/dist/index.js
@@ -1,7 +1,9 @@
 // Export components
 // Export types
 // Auto-register all primitives
-export function test() {
+function test() {
     console.log('test updated');
 }
+
+export { test };
 //# sourceMappingURL=index.js.map
