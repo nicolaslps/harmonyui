852c47c3ba466da3b4a6c311bddf09cd0f40e328
nicolaslps <lopesn.pro@gmail.com>
2025-10-22 23:22:47 +0200

    lint rector

--- apps/docs/src/Form/FormDemoType.php
+++ apps/docs/src/Form/FormDemoType.php
@@ -47,9 +47,9 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 final class FormDemoType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $formBuilder, array $options): void
     {
-        $builder
+        $formBuilder
             ->add('text', TextType::class, [
                 'label' => 'Text ',
                 'required' => false,
@@ -214,9 +214,9 @@ final class FormDemoType extends AbstractType
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
+    public function configureOptions(OptionsResolver $optionsResolver): void
     {
-        $resolver->setDefaults([
+        $optionsResolver->setDefaults([
         ]);
     }
 }
